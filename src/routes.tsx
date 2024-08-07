d396318a86e5f7518460d1a602ff198e15d9ae20
prashil-lonakar <prashil.l@volgainfotech.com>
2024-08-08 02:04:13 +0530

    pages and layout added

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from 'react-router-dom';
+import HomeLayout from './layouts/Home';
+import HomePage from './pages/Home';
+
+interface RouteConfig {
+    path: string;
+    element: React.ReactNode;
+    title?: string;
+    children?: RouteConfig[];
+}
+
+const routes = (isLogged: boolean): RouteConfig[] => [
+    {
+        path: '/',
+        element: <HomeLayout />,
+        title: 'Home',
+        children: [{ path: 'home', element: <HomePage />, title: 'main' }],
+    },
+    { path: '*', element: <Navigate to="/404" replace /> },
+];
+
+export default routes;
